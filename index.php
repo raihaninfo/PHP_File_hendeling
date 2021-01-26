9543aaff129255320e60f6b0f5d88bde2fb60866
Md Abu. Raihan <raihanmahmudi35@gmail.com>
2021-01-26 15:47:06 +0600

    file create funsion added

--- index.php
+++ index.php
@@ -8,11 +8,32 @@
     <!-- html code hear -->
         <?php 
         
+        // file read
+
+
         $ourfile = fopen("text.txt", "r") or die ("File not found");
         while(!feof($ourfile)){
             echo fgets($ourfile)."<br/>";
         } ;
         fclose($ourfile);
+
+
+        // file create
+
+        $filecreate = fopen("new.txt" , "w") or die ("File not found");
+        
+        $one = "My name is Raihan\n";
+        fwrite($filecreate, $one);
+
+        $one = "I'm Web Developer\n";
+        fwrite($filecreate, $one);
+
+        $one = "I Love Php\n";
+        fwrite($filecreate, $one);
+
+        fclose($filecreate);
+
+
         ?>
 
 
